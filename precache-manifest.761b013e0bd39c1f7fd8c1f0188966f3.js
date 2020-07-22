ddd30c8b8fea08d2b148e618114ea9a5eef41314
jesusvazquez <jesusvaz@gmail.com>
2020-07-22 14:10:54 -0600

    Updates

---
+++ precache-manifest.761b013e0bd39c1f7fd8c1f0188966f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e36a508d4a1633bbaa09538d390945",
+    "url": "/jesusvaz/bussiness.git/index.html"
+  },
+  {
+    "revision": "fcb5eac4c81ad547d5c9",
+    "url": "/jesusvaz/bussiness.git/static/css/main.d7a0b192.chunk.css"
+  },
+  {
+    "revision": "1e0ceced95521b26053d",
+    "url": "/jesusvaz/bussiness.git/static/js/2.aa4fa8f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jesusvaz/bussiness.git/static/js/2.aa4fa8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb5eac4c81ad547d5c9",
+    "url": "/jesusvaz/bussiness.git/static/js/main.0d7f1518.chunk.js"
+  },
+  {
+    "revision": "9d2dbaa36d1056b26861",
+    "url": "/jesusvaz/bussiness.git/static/js/runtime-main.53891ad9.js"
+  }
+]);
